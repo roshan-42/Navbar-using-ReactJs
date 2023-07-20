acd6cd73a8e5fac77cf1eff22e806081e68a6650
Roshan Acharya <roshanacharya207@gmail.com>
2023-07-20 21:11:47 +0545

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,10 +13,10 @@ const showNavbar = () =>{
     <header>
     <h3>Logo</h3>
     <nav ref={navRef}>
-        <a href='/#'>Home</a>
-        <a href='/#'>My work</a>
-        <a href='/#'>Blog</a>
-        <a href='/#'>About me</a>
+        <a href='/home'>Home</a>
+        <a href='/mywork'>My work</a>
+        <a href='/blog'>Blog</a>
+        <a href='/aboutme'>About me</a>
         <button className="nav-btn nav-close-btn"  onClick={showNavbar}>
             <FaTimes />
 
