5eca7d79da75e529fdb6d92d727bed95cf894ce7
Roshan Acharya <roshanacharya207@gmail.com>
2023-07-20 19:05:49 +0545

    Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import {useRef} from "react";
+import React from 'react'
+import "../Styles/main.css";
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+function Navbar(){
+const navRef = useRef();
+
+const showNavbar = () =>{
+  navRef.current.classList.toggle("responsive_nav");
+}
+  return (
+    <header>
+    <h3>Logo</h3>
+    <nav ref={navRef}>
+        <a href='/#'>Home</a>
+        <a href='/#'>My work</a>
+        <a href='/#'>Blog</a>
+        <a href='/#'>About me</a>
+        <button className="nav-btn nav-close-btn"  onClick={showNavbar}>
+            <FaTimes />
+
+        </button>
+    </nav>
+    <button className="nav-btn"  onClick={showNavbar}>
+        <FaBars />
+    </button>
+    </header>
+
+  )
+}
+
+export default Navbar;
